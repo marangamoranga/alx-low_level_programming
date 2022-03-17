8fc12a05f3d9dcd56c8c304e411ec31b2b83eb44
morangamaranga <hm.maranga@gmail.com>
2022-03-17 12:30:08 -0700

    correction 5 and 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
- */
+* more_numbers - prints 10 times the numbers, from 0 to 14.
+*/
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	char c;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (c = 0; c <= 14; c++)
 		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,13 @@
 #include "main.h"
-
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
- */
+* print_line - Write a function that draws a straight line in the terminal.
+* @n: Integer amount of _
+*/
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i;
+	char c;
 
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
-
+	for (c = 0; c < n; c++)
+		_putchar('_');
+	_putchar('\n');
 }
