2d128ee3c7d263fd0ad907fab9e947e7b77c6cf6
morangamaranga <hm.maranga@gmail.com>
2022-03-30 12:42:34 -0700

    added taskone and two

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,4 @@
-#include "man.h"
+#include "main.h"
 
 /**
  * _strlen_recursion - returns the length of a string
@@ -8,7 +8,7 @@
  */
 int _strlen_recursion(char *s)
 {
-  if (*s == '\0')
-    return (0);
-  return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
