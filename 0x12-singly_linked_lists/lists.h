b2557314897cf10456740ef30ca347959f409879
morangamaranga <hm.maranga@gmail.com>
2022-04-21 08:45:28 -0700

    updated tasks.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,7 +12,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -20,12 +20,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+unsigned int _strlen(const char *str);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-int _strlen(const char *);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-void print(void) __attribute__((constructor));
 
-#endif /*LISTS_H*/
+#endif
